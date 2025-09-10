52383deba7624d511177805e122e0904c76fb2e8
Mister Giant <63671733+mistergiantmedia@users.noreply.github.com>
2025-09-10 17:50:54 +0200

    mister giant & removed list

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -31,10 +31,10 @@ const Footer = () => {
           Powered by{" "}
           <Anchor
             size="xs"
-            href="https://github.com/stonith404/pingvin-share"
+            href="https://mistergiant.com"
             target="_blank"
           >
-            Pingvin Share
+            Mister Giant
           </Anchor>
         </Text>
         <div>
