681530a45540ec0af211b47d965eed2a862de6f6
Mister Giant <63671733+mistergiantmedia@users.noreply.github.com>
2025-09-10 15:54:57 +0200

    syle change

--- frontend/src/styles/mantine.style.ts
+++ frontend/src/styles/mantine.style.ts
@@ -1,20 +1,20 @@
 import { MantineThemeOverride } from "@mantine/core";
 
 export default <MantineThemeOverride>{
-  colors: {
-    victoria: [
-      "#E2E1F1",
-      "#C2C0E7",
-      "#A19DE4",
-      "#7D76E8",
-      "#544AF4",
-      "#4940DE",
-      "#4239C8",
-      "#463FA8",
-      "#47428E",
-      "#464379",
-    ],
-  },
+colors: {
+  victoria: [
+    "#E6FFFC", // Lightest - equivalent to the original #E2E1F1
+    "#CCFFF9", // Light - equivalent to #C2C0E7
+    "#B3FFF5", // Medium-light - equivalent to #A19DE4
+    "#00ffd2", // Base color (your chosen cyan)
+    "#00E6BC", // Medium-dark
+    "#00CCA6", // Dark
+    "#00B390", // Darker
+    "#009A7A", // Even darker
+    "#008064", // Very dark
+    "#00664E", // Darkest
+  ],
+},
   primaryColor: "victoria",
   components: {
     Modal: {
